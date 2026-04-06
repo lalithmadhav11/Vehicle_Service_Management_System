250c52a46df2f45f64a3997e9c88cd7d003ef5d0
Lalith Madhav <lalithmadhav1112@gmail.com>
2026-04-06 21:23:57 +0530

    written auth routes and controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,66 @@
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
+
+// @desc    Register a new user
+// @route   POST /api/auth/register
+// @access  Public
+export const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      return next(new Error("User already exists"));
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role: role || "customer",
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      return next(new Error("Invalid user data"));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc    Auth user & get token
+// @route   POST /api/auth/login
+// @access  Public
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      return next(new Error("Invalid email or password"));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
